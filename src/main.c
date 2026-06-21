66336d8b617374e678af2f959696582c6e09560d
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-06-22 02:42:04 +0530

    [KITTY-TABS] fix: fixed last tab default selection on escaping fzf window

--- src/main.c
+++ src/main.c
@@ -13,7 +13,10 @@ int main(void) {
 
   TabList tabs = parse_tabs(tabs_json);
   int tab_id = pick_tab(tabs.tabs, tabs.count);
-  focus_tab(tab_id);
+
+  if (tab_id != -1) {
+    focus_tab(tab_id);
+  }
 
   free(tabs_json);
   free_tabs(tabs);
