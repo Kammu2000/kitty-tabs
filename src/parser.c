dfb03947c2fea3564de9d22cb308671feddb713a
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-07-10 07:40:14 +0530

    [KITTY-TABS] chore: indexing tabs from all kitty windows

--- src/parser.c
+++ src/parser.c
@@ -9,27 +9,37 @@ TabList parse_tabs(char *tabs_json) {
   TabList result = {0};
 
   cJSON *window_list = cJSON_Parse(tabs_json);
-
   if (!window_list) {
     return result;
   }
 
   int window_count = cJSON_GetArraySize(window_list);
 
+  // Pass 1: Count total tabs
+  int total_tabs = 0;
   for (int i = 0; i < window_count; i++) {
     cJSON *window = cJSON_GetArrayItem(window_list, i);
-
-    cJSON *is_active = cJSON_GetObjectItem(window, "is_active");
-
-    if (!cJSON_IsBool(is_active) || !cJSON_IsTrue(is_active)) {
-      continue;
-    }
-
     cJSON *tabs = cJSON_GetObjectItem(window, "tabs");
-    int tab_count = cJSON_GetArraySize(tabs);
 
-    result.tabs = malloc(sizeof(Tab) * tab_count);
-    result.count = tab_count;
+    if (cJSON_IsArray(tabs)) {
+      total_tabs += cJSON_GetArraySize(tabs);
+    }
+  }
+
+  result.tabs = malloc(sizeof(Tab) * total_tabs);
+  result.count = total_tabs;
+
+  // Pass 2: Copy tabs
+  int index = 0;
+
+  for (int i = 0; i < window_count; i++) {
+    cJSON *window = cJSON_GetArrayItem(window_list, i);
+    cJSON *tabs = cJSON_GetObjectItem(window, "tabs");
+
+    if (!cJSON_IsArray(tabs))
+      continue;
+
+    int tab_count = cJSON_GetArraySize(tabs);
 
     for (int j = 0; j < tab_count; j++) {
       cJSON *tab = cJSON_GetArrayItem(tabs, j);
@@ -37,16 +47,13 @@ TabList parse_tabs(char *tabs_json) {
       cJSON *id = cJSON_GetObjectItem(tab, "id");
       cJSON *title = cJSON_GetObjectItem(tab, "title");
 
-      result.tabs[j].id = id ? id->valueint : -1;
+      result.tabs[index].id = id ? id->valueint : -1;
+      result.tabs[index].title = (title && cJSON_IsString(title))
+                                     ? strdup(title->valuestring)
+                                     : strdup("");
 
-      if (title && cJSON_IsString(title)) {
-        result.tabs[j].title = strdup(title->valuestring);
-      } else {
-        result.tabs[j].title = strdup("");
-      }
+      index++;
     }
-
-    break;
   }
 
   cJSON_Delete(window_list);
