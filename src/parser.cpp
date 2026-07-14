5aeb5e609f2f8b0edfc0aca08bdeae813db2f492
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-07-15 01:13:29 +0530

    [KITTY-TABS] feat: Added parser logic

---
+++ src/parser.cpp
@@ -0,0 +1,27 @@
+#include <nlohmann/json.hpp>
+#include <vector>
+
+#include "parser.hpp"
+
+using json = nlohmann::json;
+
+std::vector<Tab> parse_tabs(const std::string &window_list_json) {
+  std::vector<Tab> result;
+
+  auto window_list = json::parse(window_list_json);
+
+  if (window_list.is_array()) {
+    for (const auto &window : window_list) {
+      auto tabs = window["tabs"];
+
+      if (tabs.is_array()) {
+        for (const auto &tab : tabs) {
+          Tab tb{tab["id"], tab["title"]};
+          result.push_back(tb);
+        }
+      }
+    }
+  }
+
+  return result;
+}
