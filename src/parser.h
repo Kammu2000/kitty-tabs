5f41fa7fa006addb0a69982d91985ad404e2ba00
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-07-14 13:21:24 +0530

    [KITTY-TABS] chore: removed all c files

--- src/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "types.h"
-
-TabList parse_tabs(char *tabs_json);
-void free_tabs(TabList list);
-
-#endif
--- src/process.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROCESS_H
-#define PROCESS_H
-
-void run_command(char *const argv[]);
-char *run_command_with_input(char *const argv[], const char *input);
-char *capture_command_output(char *const argv[]);
-
-#endif
