5fcbf61fc513e95fad55302a056a8bde384788ca
kammu2000 <deepanshuupadhyay1310@gmail.com>
2026-07-15 01:10:30 +0530

    [KITTY-TABS] feat: Implemented RAII wrapper class for pipe

---
+++ include/pipe.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <array>
+
+class Pipe {
+public:
+  Pipe();
+  Pipe(const Pipe &) = delete;
+  Pipe &operator=(const Pipe &) = delete;
+  Pipe(Pipe &&) noexcept;
+  Pipe &operator=(Pipe &&) noexcept;
+  ~Pipe();
+
+  int read_fd() const;
+  int write_fd() const;
+  void close_read();
+  void close_write();
+
+private:
+  inline void close_fd() {
+    close_read();
+    close_write();
+  }
+
+  std::array<int, 2> fd{-1, -1};
+};
